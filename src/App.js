fc40256fd705371656c741defee572eeb427996d
Jake Grether <jgrether@Jakes-MBP.hsd1.ut.comcast.net>
2021-09-02 12:06:43 -0600

    hashrouter and textarea fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { HashRouter, Route, Redirect } from 'react-router-dom'
 
 //Custom Components
 import Home from './pages/Home'
@@ -13,7 +13,7 @@ import './App.css'
 
 const App = () => {
   return (
-    <Router>
+    <HashRouter>
       <div className='site-bground'>
         <MainNav />
         <div id='site-container'>
@@ -35,7 +35,7 @@ const App = () => {
           {/* </Switch> */}
         </div>
       </div>
-    </Router>
+    </HashRouter>
   )
 }
 
